9beb512bf86633424825e2f3ec30dffb5fe7ea68
Qianhui-Lin <qianhuilin33@gmail.com>
2026-06-27 11:42:57 +0100

    feat: ran an example scenario

--- src/VersionModule.f90
+++ src/VersionModule.f90
@@ -1,4 +1,4 @@
 module VersionModule
     implicit none
-    character(len=17), parameter :: MODEL_VERSION = "0.0.4" 
+    character(len=17), parameter :: MODEL_VERSION = "0.0.5-6-gfa0f50e" 
 end module VersionModule
